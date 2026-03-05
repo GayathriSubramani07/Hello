1d58af09100d20e4a8d7e750def5716f3c590052
gayathri <gayathrivelox@gmail.com>
2026-03-05 16:16:41 +0530

    Updated

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
 public class Hello {
-    public static void main(String args[]) {
-        for(int i = 0; i <= 10; i++) {
-            System.out.print(i + " "+ "hello world");
+    public static void main(String[] args) {
+
+        int num = 5;
+        int fact = 1;
+
+        for(int i = 1; i <= num; i++) {
+            fact = fact * i;
         }
+
+        System.out.println("Factorial of " + num + " is " + fact);
     }
 }
